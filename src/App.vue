86b5b3e80f3771f87fbcb06bdf0b7c671c8511eb
unknown <n.niedziela201@gmail.com>
2020-10-20 17:59:59 +0200

    add welcome screen

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-  
+ <div id="app">
+     <button @click="isStarted = !isStarted">Start</button>
+     <section v-if="isStarted === true">
+        <p>lalal</p>
+     </section>
+ </div>
 </template>
 
 <script>
 export default {
- 
+  data () {
+    return {
+      isStarted: false
+    }
+  }
 }
 </script>
 
